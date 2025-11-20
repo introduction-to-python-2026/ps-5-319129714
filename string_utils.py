1ed29310959a7e6daad956a8a8f08658ef3198a6
319129714 <roishvimer@gmail.com>
2025-11-20 12:43:13 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -42,53 +42,48 @@ def count_atoms_in_molecule(molecular_formula):
     return atom_counts
 
 
+def parse_chemical_reaction(reaction_equation):
+    reaction_equation = reaction_equation.replace(" ", "")
+    reactants, products = reaction_equation.split("->")
+    return reactants.split("+"), products.split("+")
+
+
 def balance_reaction(equation):
-    """
-    Balances a chemical equation using linear algebra.
-    Example: "Fe2O3 + H2 -> Fe + H2O" -> [0.333, 1.0, 0.666, 1.0]
-    """
-    # 1. Parse the equation into reactants and products list
-    reactants_str, products_str = equation.split('->')
-    reactants = [m.strip() for m in reactants_str.split('+')]
-    products = [m.strip() for m in products_str.split('+')]
+    
+    reactants, products = parse_chemical_reaction(equation)
     all_molecules = reactants + products
     
-    # 2. Get atom counts for every molecule
     molecule_compositions = [count_atoms_in_molecule(m) for m in all_molecules]
     
-    # 3. Find all unique elements in the reaction
     all_elements = set()
     for m in molecule_compositions:
         all_elements.update(m.keys())
     all_elements = sorted(list(all_elements))
     
-    # 4. Build the Matrix for linear equation system
-    # Rows = elements, Columns = molecules
     matrix = []
     for el in all_elements:
         row = []
         for i, mol_counts in enumerate(molecule_compositions):
             count = mol_counts.get(el, 0)
-            # Products (right side) need to be negative to satisfy (Reactants - Products = 0)
             if i >= len(reactants):
                 count = -count
             row.append(count)
         matrix.append(row)
     
-    # 5. Solve using NumPy
-    # We assume the coefficient of the LAST molecule is 1.0, and solve for the rest.
-    # Ax = 0  =>  A_partial * x_partial = - (Last_Column * 1)
-    
     matrix_np = np.array(matrix)
     
-    # Split into coefficients to solve (A) and the dependent column (b)
     a_matrix = matrix_np[:, :-1]
     b_vector = -matrix_np[:, -1]
     
-    # Use Least Squares to solve (works even if matrix is not square)
     coeffs, residuals, rank, s = np.linalg.lstsq(a_matrix, b_vector, rcond=None)
     
-    # Add the last coefficient (which we fixed as 1.0)
     result = list(coeffs) + [1.0]
     
     return result
+
+
+def count_atoms_in_reaction(molecules_list):
+    molecules_atoms_count = []
+    for molecule in molecules_list:
+        molecules_atoms_count.append(count_atoms_in_molecule(molecule))
+    return molecules_atoms_count
